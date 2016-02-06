87144f51157d038ade2428f5f089d4189a48a1f5
james2doyle <james2doyle@gmail.com>
2016-02-06 14:42:30 -0800

    bump version to avoid lit conflict

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "james2doyle/string-trim",
-  version = "0.0.2",
+  version = "0.0.3",
   homepage = "https://github.com/james2doyle/lit-string-trim",
   description = "Trim the leading and trailing whitespace from a string",
   tags = {
